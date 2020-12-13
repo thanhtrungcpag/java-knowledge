8c3d70a246c021ebe68283464f96ee282847f995
unknown <trungcpag@gmail.com>
2020-12-13 17:32:27 +0700

    Demo code MVC

---
+++ basic/BasicWebApplication/SpringMVCDemo/src/main/java/com/example/SpringMVCDemo/SpringMvcDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringMVCDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoApplication {
+	
+	public static void main(String[] args) {
+		System.out.println("Helo");
+		SpringApplication.run(SpringMvcDemoApplication.class, args);
+	}
+
+}
